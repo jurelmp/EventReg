0e202130889e5ba10054863a72a256f24faf92fe
patoc.jp <patoc.jm@sprobe.com>
2017-08-10 15:40:52 +0800

    Using $http Promises

---
+++ app/js/services/EventData.js
@@ -0,0 +1,10 @@
+eventsApp.factory('eventData', function ($http) {
+    return {
+        getEvent: function () {
+            return $http({
+                method: 'GET',
+                url: '/data/event/1'
+            });
+        }
+    };
+});
