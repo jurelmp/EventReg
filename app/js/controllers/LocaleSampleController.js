cd12b92a9c8721e6edc6e3d4323bfe022fb14f42
patoc.jp <patoc.jm@sprobe.com>
2017-08-11 13:11:27 +0800

    Using Angular's $exceptionHandler Service

--- app/js/controllers/LocaleSampleController.js
+++ app/js/controllers/LocaleSampleController.js
@@ -2,8 +2,9 @@
 
 eventsApp.controller('LocaleSampleController',
     function LocaleSampleController($scope, $locale) {
-
+        console.log($locale);
         $scope.myDate = Date.now();
         $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
 
+        // throw {message: 'error message'};
     });
